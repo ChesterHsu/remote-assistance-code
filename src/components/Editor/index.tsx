a17e9d08f851749792d99abb667349da334444f5
Chester <aa0909286667@gmail.com>
2022-12-28 18:08:58 +0800

    add（新增Editor）: 第一版

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,6 +1,10 @@
+import '@/css/components/Editor/index.scss'
+
 function Editor() {
     return(
         <div className={`editor`}>
+            <div className={`sequence`}></div>
+            <div className={`edit`}></div>
         </div>
     )
 }
