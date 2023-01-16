229a4bcb2b17cf43c360040673bfc087673354a8
@aa0909286667 <aa0909286667@gmail.com>
2023-01-16 13:06:36 +0800

    實作功能選項 第一階段 功能實作 （未優化組件）

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,22 @@ import { isDark } from '@/tools/judge';
 import closeRightMouseButton from '@/tools/closeRightMouseButton';
 
 function App() {
+  //使用 node.js 在本機上執行的 Server
+  let ws = new WebSocket('ws://localhost:21274')
+
+  //開啟後執行動作
+  //指定一個function在onopen後執行
+  //順便也把websocket物件印出來觀察看看
+  ws.onopen = () => {
+    console.log(ws)
+    console.log('開啟連結')
+  }
+
+  //關閉後執行動作
+  //指定一個function在onclose後執行
+  ws.onclose = () => {
+    console.log('關閉連結')
+  }
   /**
    * 取得預設Theme模式
    * **/
